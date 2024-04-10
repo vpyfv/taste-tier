edea9c6c200272a91ea63f3906b2e13fbad87717
vjkrsh <33751890+vjkrsh@users.noreply.github.com>
2024-04-10 11:35:37 -0500

    added firebase file

---
+++ app/server/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { connectFirestoreEmulator, getFirestore } from "@firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
+};
+
+// Initialize Firebase
+const FirebaseApp = initializeApp(firebaseConfig);
+export const FirebaseDB = getFirestore(FirebaseApp);
+connectFirestoreEmulator(FirebaseDB, "127.0.0.1", 8080);
+export const firebaseAuth = getAuth(FirebaseApp);
+connectAuthEmulator(firebaseAuth, "http://127.0.0.1:9099");
